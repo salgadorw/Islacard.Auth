18eb17c0f0da0ef4f8d99405ba5b8e5f01696d3e
salgadorw <rafaelwsalgado@gmail.com>
2023-03-27 10:23:02 +0100

    fix on react app api ports

--- Frontend/LoginFE/src/components/Login/Login.tsx
+++ Frontend/LoginFE/src/components/Login/Login.tsx
@@ -10,7 +10,7 @@ function Login(){
     const handleSubmit = (e: React.SyntheticEvent)=> {        
         e.preventDefault();       
         async function fetchData(){              
-            const response = await fetch('https://localhost:7260/api/Auth', {
+            const response = await fetch('http://localhost:8080/api/Auth', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
